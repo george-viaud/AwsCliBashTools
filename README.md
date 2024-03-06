a53e93fef0960296a8542e5b690a5ec4ca9d4674
george <george@mediaforce.com>
2024-03-05 20:45:15 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # AwsCliBashTools
 Useful scripts for use with aws-cli
+
+## Prerequisites
+- Ensure AWS CLI is installed and configured with the necessary permissions.
+- Ensure `jq` is installed for processing JSON data.
+
+## Suggested Installation
+
+To use this script from anywhere on your system, it's recommended to move it to a central location and then include that location in your system's `PATH`.
+
+### Moving the Script
+
+Move the script to a suitable directory, such as `/opt/`:
+```bash
+sudo mv AwsCliBashTools /opt/
+```
+
+### Updating the PATH
+
+Add the script's location to your `PATH` by appending it to your `~/.bashrc` or `~/.zshrc` file:
+```bash
+echo 'export PATH="$PATH:/opt/AwsCliBashTools"' >> ~/.bashrc
+```
+Then, source your `.bashrc` or `.zshrc` to apply the changes:
+```bash
+source ~/.bashrc
+```
+
+***
+## asg-refresh.sh
+
+A Bash script for easily triggering refresh actions on AWS Auto Scaling Groups (ASGs) via the command line, with support for filtering ASGs based on tags.
+### Basic Usage
+Invoke the script with the `--tags` option followed by a comma-separated list of tag key-value pairs to filter the ASGs:
+
+```bash
+./asg-refresh.sh --tags="Environment=Production,App=Analytics"
+```
+
+This command will fetch all ASGs with the specified tags and ask to initiate a refresh action on them as a group.
+
+### Individual-Match Confirmation
+To manually confirm each ASG refresh, use the `--ask-each` flag:
+
+```bash
+./asg-refresh.sh --tags="Environment=Production,App=Analytics" --ask-each
+```
+
+The script will prompt for confirmation for each individual ASG matched.
+
+***
---
+++ asg-refresh.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Script Name: Autoscaling Group Refresh
+# Description: Easy triggering of ASG refresh via cli
+# https://github.com/george-viaud/AwsCliBashTools
+# Author: George Viaud
+# Year: 2024
+# License: MIT License (see LICENSE https://opensource.org/licenses/MIT)
+
+SCRIPT_NAME=$(basename "$0")
+
+# ANSI color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+# Function to parse --tags and --ask-each arguments
+parse_args() {
+    for arg in "$@"
+    do
+        case $arg in
+            --tags=*)
+            TAGS="${arg#*=}"
+            ;;
+            --ask-each)
+            ASK_EACH=true
+            ;;
+        esac
+    done
+}
+
+# Function to fetch ASGs based on provided tags and display their names
+fetch_and_display_asgs() {
+    echo -e "${BLUE}Fetching Auto Scaling Groups matching tags...${NC}"
+
+    JQ_FILTER='.AutoScalingGroups[] | select('
+    FIRST_TAG=true
+
+    IFS=',' read -r -a TAG_ARRAY <<< "$TAGS"
+    for TAG_PAIR in "${TAG_ARRAY[@]}"; do
+        IFS='=' read -r KEY VALUE <<< "$TAG_PAIR"
+        if [ "$FIRST_TAG" = true ]; then
+            FIRST_TAG=false
+        else
+            JQ_FILTER+=' and '
+        fi
+        JQ_FILTER+="(any(.Tags[]; .Key == \"$KEY\" and .Value == \"$VALUE\"))"
+    done
+    JQ_FILTER+=') | .AutoScalingGroupName'
+
+    ASG_NAMES=$(aws autoscaling describe-auto-scaling-groups | jq -r "$JQ_FILTER")
+
+    if [ -z "$ASG_NAMES" ]; then
+        echo -e "${RED}No matching ASGs found.${NC}"
+        exit 1
+    fi
+
+    echo -e "${GREEN}Found matching ASGs:${NC}"
+    echo "$ASG_NAMES"
+}
+
+# Function to start the instance refresh
+start_instance_refresh() {
+    echo -e "${YELLOW}Starting instance refresh for ASG: $1.${NC}"
+    aws autoscaling start-instance-refresh --auto-scaling-group-name "$1" \
+        --strategy "Rolling" \
+        --preferences '{
+            "MinHealthyPercentage": 100,
+            "MaxHealthyPercentage": 110,
+            "InstanceWarmup": 300
+        }'
+    echo -e "${GREEN}Instance refresh initiated for $1.${NC}"
+}
+
+# Main script logic starts here
+TAGS=""
+ASK_EACH=false
+
+parse_args "$@"
+
+if [ -z "$TAGS" ]; then
+    echo -e "${RED}Use: $SCRIPT_NAME --tags=\"Key1=Value1, ...\" [--ask-each] ${NC}"
+    exit 1
+fi
+
+fetch_and_display_asgs
+
+if [ "$ASK_EACH" = true ]; then
+    for ASG_NAME in $ASG_NAMES; do
+        read -p "$(echo -e "${YELLOW}Proceed with instance refresh for $ASG_NAME? (y/N):${NC}") " confirm < /dev/tty
+        if [[ $confirm == [yY] ]]; then
+            start_instance_refresh "$ASG_NAME"
+        else
+            echo -e "${RED}Skipped instance refresh for $ASG_NAME.${NC}"
+        fi
+    done
+else
+    read -p "$(echo -e "${YELLOW}Proceed with instance refresh for all above ASGs? (y/N):${NC}") " confirm_all < /dev/tty
+    if [[ $confirm_all == [yY] ]]; then
+        for ASG_NAME in $ASG_NAMES; do
+            start_instance_refresh "$ASG_NAME"
+        done
+    else
+        echo -e "${RED}Instance refresh canceled for all ASGs.${NC}"
+    fi
+fi
+
+echo -e "${GREEN}Process completed.${NC}"
